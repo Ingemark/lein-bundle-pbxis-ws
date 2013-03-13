896c96217e08696a8c61f9afe5eb96415d45659e
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-13 15:22:40 +0100

    SNAPSHOT version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/lein-bundle-pbxis-ws "0.1.0"
+(defproject com.ingemark/lein-bundle-pbxis-ws "0.1.1-SNAPSHOT"
   :description "lein-bundle helper for pbxis-ws"
   :url "https://github.com/Inge-mark/bundle-pbxis-ws"
   :license {:name "Eclipse Public License"
