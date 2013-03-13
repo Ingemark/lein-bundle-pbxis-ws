27e3fa6d1367591171a8302e0b678a8c60d53cc9
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-13 15:19:52 +0100

    Initial

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject com.ingemark/lein-bundle-pbxis-ws "0.1.0"
+  :description "lein-bundle helper for pbxis-ws"
+  :url "https://github.com/Inge-mark/bundle-pbxis-ws"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in :leiningen
+  :lein-release {:deploy-via :clojars}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]])
+
---
+++ src/leiningen/bundle_pbxis_ws.clj
@@ -0,0 +1,25 @@
+(ns leiningen.bundle-pbxis-ws
+  (require [leiningen.uberjar :refer [uberjar]]
+           (clojure.java [shell :as sh] [io :as io])
+           [clojure.string :as s]))
+
+(set! *warn-on-reflection* true)
+
+(defn raise [fmt & args] (throw (RuntimeException. ^String (apply format fmt args))))
+
+(defn sh! [& args]
+  (apply println "$" args)
+  (let [res (apply sh/sh args)]
+    (print (:out res)) (print (:err res)) (flush)
+    (when-not (zero? (:exit res)) (raise "Command failed with exit code %s: %s" (:exit res) args))))
+
+(defn bundle-pbxis-ws [project]
+  (let [tarfile (format "pbxis-ws-%s.tgz" (:version project))
+        jarfile "pbxis-ws-standalone.jar"]
+    (println "lein uberjar")
+    (sh! "mv" (uberjar project) jarfile)
+    (sh! "tar" "cvfz" tarfile
+         jarfile
+         "pbxis-config.clj.template"
+         "logback.xml")
+    (sh! "mv" tarfile "..")))
