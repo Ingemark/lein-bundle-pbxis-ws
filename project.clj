9b405b093f93038512a284c39649e44add455e3b
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-19 11:47:11 +0100

    Move to 0.1.4-SNAPSHOT (by lein-release)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/lein-bundle-pbxis-ws "0.1.3"
+(defproject com.ingemark/lein-bundle-pbxis-ws "0.1.4-SNAPSHOT"
   :description "lein-bundle helper for pbxis-ws"
   :url "https://github.com/Inge-mark/bundle-pbxis-ws"
   :license {:name "Eclipse Public License"
